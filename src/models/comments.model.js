8b31a4d9527c8f5fc4264f5ad4d8efe26cb6a3ea
Mems2001 <marioed95@hotmail.com>
2022-12-31 14:07:27 -0500

    debugging

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,5 +1,5 @@
-const db = require('../utils/database');
 const {DataTypes} = require('sequelize');
+const db = require('../utils/database');
 const Users = require('./users.model');
 const Posts = require('./posts.model');
 
